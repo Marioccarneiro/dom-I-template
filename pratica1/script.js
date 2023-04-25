e470b5b12bf09f70df92ccf4045fcfc4cbc002aa
Marioccarneiro <mario.ccarneiro@hotmail.com>
2023-04-24 21:27:20 -0300

    dom-I-template

---
+++ pratica1/script.js
@@ -0,0 +1,30 @@
+const sapo = document.getElementById('sapo')
+
+// console.log(sapo)
+// console.log(sapo.innerHTML)
+
+sapo.innerHTML += ' pé'
+
+console.log(sapo)
+
+//
+
+const poema = document.getElementById('poema')
+poema.innerHTML += ' evidencias'
+console.log(poema)
+
+const sabedoria = document.getElementById('sabedoria')
+sabedoria.innerHTML += ' voando'
+console.log(sabedoria)
+
+const borboleta = document.getElementById('borboleta')
+borboleta.innerHTML += ' picapau'
+console.log(borboleta)
+
+const roberto = document.getElementById('roberto')
+roberto.innerHTML += ' vivi'
+console.log(roberto)
+console.log(roberto.innerHTML)
+
+//
+
